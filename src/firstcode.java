26101b6c703cd0452481aca816b29948ba701265
Minasyankarine <59695225+Minasyankarine@users.noreply.github.com>
2020-01-14 11:43:06 +0400

    Update firstcode.java

--- src/firstcode.java
+++ src/firstcode.java
@@ -7,7 +7,7 @@ public class firstcode {
         {
 
             for(int i=0; i<10; i++)
-            System.out.println("Hello  World Karine "+i);
+            System.out.println("Hello  World Karine Minasyan "+i);
 
         }
 
