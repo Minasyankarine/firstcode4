6cab61bf392454c589617e3402dbe01e0e4385b5
Minasyankarine <59695225+Minasyankarine@users.noreply.github.com>
2020-01-14 14:26:53 +0400

    Update firstcode.java

--- src/firstcode.java
+++ src/firstcode.java
@@ -1,17 +1,7 @@
 public class firstcode {
 
-
-
-        public static void main (String args[])
-
-        {
-
-            for(int i=0; i<10; i++)
-            {
-            System.out.println("Hello world "+i);
-            }
-
-        }
-
-
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
